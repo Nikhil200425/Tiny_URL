5928891a04ecfe946458b9148d9238ea19bea75b
trainee.prempatil@allerin.com <trainee.prempatil@allerin.com>
2025-08-12 17:46:34 +0530

    tiny-url-commit

---
+++ app/models/link.rb
@@ -0,0 +1,10 @@
+class Link < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 2 }, uniqueness: true
+    validates :hyperlink, presence: true, format: URI::regexp(%w[http https]), uniqueness: true
+    before_create :generate_tiny_code
+    private
+    def generate_tiny_code
+        special_char = ['@', '#', '$', '%', '^', '&', '*']
+        self.tiny_code = SecureRandom.alphanumeric(3) + special_char.sample + SecureRandom.alphanumeric(4)
+    end
+end
---
+++ app/views/links/_link.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! link, :id, :title, :hyperlink, :tiny_code, :created_at, :updated_at
+json.url link_url(link, format: :json)
